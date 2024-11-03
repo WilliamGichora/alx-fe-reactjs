73c576120b10f48fedf9ff0e10ba107cf895459e
Mark Gichora <markgichora7@gmail.com>
2024-11-03 22:40:03 +0300

    Adden Header,Main and Footer components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,4 +1,7 @@
 import WelcomeMessage from './components/WelcomeMessage'
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+import Footer from './components/Footer'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -9,7 +12,12 @@ function App() {
 
   return (
     
-      <WelcomeMessage />
+    <>
+      <Header />
+      <MainContent />
+      <Footer />
+    </>
+    
       
   )
 }
