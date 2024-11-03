15e9d431ea2e83a251e93af1784834b4ffc3f95c
Mark Gichora <markgichora7@gmail.com>
2024-11-03 22:23:09 +0300

    minor fix

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -8,9 +8,9 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    
       <WelcomeMessage/>
-      {/*<div>
+      /*<div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -29,9 +29,9 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>*/}
+      </p>*/
 
-    </>
+    
     
   )
 }
