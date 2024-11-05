fb61a1ef47bb485da2e5e4ac7c0c41ba93f81ce4
Mark Gichora <markgichora7@gmail.com>
2024-11-05 09:36:01 +0300

    added welcom message minor fix

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -14,6 +14,7 @@ function App() {
   return (
     
     <>
+      <WelcomeMessage />    
       <Header />
       <MainContent />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
