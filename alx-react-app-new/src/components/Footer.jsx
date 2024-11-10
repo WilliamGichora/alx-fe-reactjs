b2eb1826ed53db588e7ea2155f21d874fda67164
Mark Gichora <markgichora7@gmail.com>
2024-11-10 16:27:55 +0300

    Added inline styles

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+function Footer() {
+    return ( 
+        <footer style={{
+            fontFamily: 'sans-serif', 
+            color:'royalblue'
+        }}>
+            <p style={{fontStyle:'italic'}}>© 2023 City Lovers</p>
+        </footer>
+     );
+}
+
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,12 @@
+function MainContent() {
+    return (
+        <main style={{
+            fontSize: '20px',
+            backgroundColor:'chartreuse'
+        }}>
+            <p style={{ fontStyle:'italic'}}>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    );
+}
+
+export default MainContent;
