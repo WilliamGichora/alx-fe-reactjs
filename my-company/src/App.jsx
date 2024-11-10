8a604db20ac5234da6c4b5400867ecc238902c45
Mark Gichora <markgichora7@gmail.com>
2024-11-10 23:56:04 +0300

    Minor fix

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -3,7 +3,7 @@ import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import Home from './Home'
 import About from './About'
 import Services from './Services'
-import Contact from './Contact'
+import Contact from './components/Contact'
 import Navbar from './components/Navbar'
 import Footer from './Footer'
 
