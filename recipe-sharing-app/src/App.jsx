99ffb2eaa627eb488807d9c7dca693521e41f2c3
Mark Gichora <markgichora7@gmail.com>
2024-11-17 23:26:16 +0300

    minor fix

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -2,7 +2,7 @@ import AddRecipeForm from "./components/AddRecipeForm";
 import RecipeList from "./components/RecipeList";
 import RecipeDetails from "./components/RecipeDetails";
 import SearchBar from "./components/SearchBar";
-import FavoritesList from "./components/FavouritesList";
+import FavoritesList from "./components/FavoritesList";
 import RecommendationsList from "./components/ReccomendationsList";
 import { BrowserRouter, Routes, Route, useParams } from 'react-router-dom';
 
