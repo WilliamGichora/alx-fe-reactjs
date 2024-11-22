829ba5e5941b2eec810b53921e88ce424215fafe
Mark Gichora <markgichora7@gmail.com>
2024-11-22 18:59:41 +0300

    minor fix

--- form-handling-react/src/components/formikForm.js
+++ form-handling-react/src/components/formikForm.js
@@ -4,7 +4,7 @@ import * as Yup from 'yup';
 const SignUpSchema = Yup.object().shape({
     userName: Yup.string().min(2, "Too Short").max(50, "Too Long").required('required'),
     email: Yup.string().email('Invalid Email').required('required'),
-    password: Yup.string().min(8, "Password must be more than 8 characters").required('required'),
+    password: Yup.string().required('required'),
 });
 
 export const formikForm = () => (
