bd5b6b4f2d3931a19c3c66f8388b9826e331ba93
Mark Gichora <markgichora7@gmail.com>
2024-11-22 19:36:41 +0300

    implemented use query for data handling, and optimization through caching

---
+++ react-query-demo/src/App.jsx
@@ -0,0 +1,16 @@
+import {QueryClientProvider,QueryClient} from 'react-query'
+import './App.css'
+import PostsComponent from './components/PostsComponent'
+
+function App() {
+
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <PostsComponent/>
+    </QueryClientProvider>
+  )
+}
+
+export default App
---
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -0,0 +1,32 @@
+import {useQuery} from 'react-query'
+
+const fetchData = async () => {
+    const response = await fetch(' https://jsonplaceholder.typicode.com/posts');
+    return response.json();
+}
+
+function PostsComponent() {
+
+    const { data, error, isLoading,refetch } = useQuery('posts', fetchData,{
+        staleTime: 5000,
+    });
+
+    if (isLoading) return <div>Loading...</div>;
+    if (error) return <div>Error: {error.message}</div>;
+
+    return (
+        <div>
+            <h1>Posts</h1>
+            <button onClick={refetch}>Refetch</button>
+            <ul>
+                {data.map(post => (
+                    <li key={post.id}>
+                        <strong>{post.title }</strong>{post.body}   
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+export default PostsComponent
