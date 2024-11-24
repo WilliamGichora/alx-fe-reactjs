85503f0d2c1806577f9ca68ae9937bfb7905f6c1
Mark Gichora <markgichora7@gmail.com>
2024-11-24 23:57:00 +0300

    Added tests

---
+++ react-todo/src/components/AddTodoForm.jsx
@@ -0,0 +1,21 @@
+const AddTodoForm = ({ addTodo }) => {
+    const [text, setText] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        addTodo(text);
+        setText('');
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                type="text"
+                value={text}
+                placeholder="Add Todo"
+                onChange={(e) => setText(e.target.value)}
+            />
+            <button type="submit">Add Todo</button>
+        </form>
+    );
+};
