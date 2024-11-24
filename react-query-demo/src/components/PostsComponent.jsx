67f3991c4c16494088e789c0d33f2ac272ebed85
Mark Gichora <markgichora7@gmail.com>
2024-11-24 22:32:14 +0300

    minor fix

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -1,14 +1,27 @@
-import {useQuery} from 'react-query'
+import { useQuery } from 'react-query';
 
+// Function to fetch data
 const fetchPosts = async () => {
-    const response = await fetch(' https://jsonplaceholder.typicode.com/posts');
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+    if (!response.ok) {
+        throw new Error('Network response was not ok');
+    }
     return response.json();
-}
+};
 
 function PostsComponent() {
-
-    const { data, error, isLoading,isError,refetch } = useQuery('posts', fetchPosts,{
+    const {
+        data,
+        isLoading,
+        isError,
+        error,
+        refetch,
+        isFetching,
+    } = useQuery('posts', fetchPosts, {
         staleTime: 5000,
+        cacheTime: 10000, 
+        refetchOnWindowFocus: false,
+        keepPreviousData: true, // Retains old data while new data is loading
     });
 
     if (isLoading) return <div>Loading...</div>;
@@ -17,16 +30,18 @@ function PostsComponent() {
     return (
         <div>
             <h1>Posts</h1>
-            <button onClick={refetch}>Refetch</button>
+            <button onClick={refetch} disabled={isFetching}>
+                {isFetching ? 'Fetching...' : 'Refetch Posts'}
+            </button>
             <ul>
                 {data.map(post => (
                     <li key={post.id}>
-                        <strong>{post.title }</strong>{post.body}   
+                        <strong>{post.title}</strong>: {post.body}
                     </li>
                 ))}
             </ul>
         </div>
-    )
+    );
 }
 
-export default PostsComponent
+export default PostsComponent;
