496c00a68e7d0ca765b405cb0209edb3101e9045
Mark Gichora <markgichora7@gmail.com>
2024-12-08 14:25:43 +0300

    Added link tag

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import {  useNavigate } from "react-router-dom";
+import {  Link, useNavigate } from "react-router-dom";
 
 function HomePage() {
     const [data, setData] = useState([]);
@@ -31,6 +31,7 @@ function HomePage() {
                         <h2 className="sm:text-lg md:text-xl text-blue-800 my-4 hover:text-blue-500">{recipe.title}</h2>
                         <p className="font-montserrat font-semibold ">{recipe.summary}</p>
                         <div className="mb-3" ><img src={recipe.image} alt={recipe.title} className="rounded-xl" /></div>
+                        <Link to={`/recipe/${recipe.id}`} className="text-green-700">See Details</Link>
                     </div>
                 ))}
             </div>
